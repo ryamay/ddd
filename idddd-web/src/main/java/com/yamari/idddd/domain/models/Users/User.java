ca0e8581d4e07c83782610182a1c7f814663726a
Ryuji Yamamoto <yamamoto.ryuji.links@gmail.com>
2020-02-24 23:18:37 +0900

    add method to User(to treat User as ENTITY)

--- idddd-web/src/main/java/com/yamari/idddd/domain/models/Users/User.java
+++ idddd-web/src/main/java/com/yamari/idddd/domain/models/Users/User.java
@@ -1,11 +1,39 @@
 package com.yamari.idddd.domain.models.Users;
 
+import java.util.Objects;
+
 class User {
-	public UserId id;
-	public UserName name;
+	private UserId id;
+	private UserName name;
 		
 	public User(UserId id, UserName name) {
+		if (id == null) {
+			throw new IllegalArgumentException("idは必ず入力してください。");
+		}
+		
 		this.id = id;
+		this.changeName(name);
+	}
+		
+	public void changeName(UserName name) {
+		if (name == null) {
+			throw new IllegalArgumentException("nameは必ず入力してください。");
+		}
 		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) { return true;	}
+		if (obj == null) { return false; }
+		if (!(obj instanceof User)) { return false;	}
+		
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
 }
