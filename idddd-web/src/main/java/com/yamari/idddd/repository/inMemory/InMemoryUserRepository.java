976080328ebadf291f1d4c7caf2c32853b0bf47c
Ryuji Yamamoto <yamamoto.ryuji.links@gmail.com>
2020-03-04 00:03:31 +0900

    テスト用のインメモリリポジトリを作成

---
+++ idddd-web/src/main/java/com/yamari/idddd/repository/inMemory/InMemoryUserRepository.java
@@ -0,0 +1,33 @@
+package com.yamari.idddd.repository.inMemory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.yamari.idddd.domain.models.users.IUserRepository;
+import com.yamari.idddd.domain.models.users.User;
+import com.yamari.idddd.domain.models.users.UserId;
+import com.yamari.idddd.domain.models.users.UserName;
+
+public class InMemoryUserRepository implements IUserRepository
+{
+
+	public Map<UserId, User> store = new HashMap<UserId, User>();
+	
+	@Override
+	public void save(User user){
+		//保存時はディープコピー
+		store.put(user.id, clone(user));
+	}
+
+	@Override
+	public User find(UserName name){
+		return store.values().stream()
+			.filter((i -> name.equals(i.name)))
+			.findFirst()
+			.orElse(null);
+	}
+	
+	private User clone(User user) {
+		return new User(user.id, user.name);
+	}
+}
