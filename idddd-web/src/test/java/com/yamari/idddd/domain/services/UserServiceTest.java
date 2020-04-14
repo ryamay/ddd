b1f2717f33a8afea21c5869f786e2a484d822a85
Ryuji Yamamoto <yamamoto.ryuji.links@gmail.com>
2020-04-14 23:48:36 +0900

    MODIFY:UserServiseTestをassertJを利用するよう変更

---
+++ idddd-web/src/test/java/com/yamari/idddd/domain/services/UserServiceTest.java
@@ -0,0 +1,38 @@
+package com.yamari.idddd.domain.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.Test;
+import com.yamari.idddd.domain.models.users.MailAddress;
+import com.yamari.idddd.domain.models.users.User;
+import com.yamari.idddd.domain.models.users.UserId;
+import com.yamari.idddd.domain.models.users.UserName;
+import com.yamari.idddd.repository.inMemory.InMemoryUserRepository;
+
+public class UserServiceTest {
+
+  public InMemoryUserRepository testRepository = new InMemoryUserRepository();
+
+  public UserService targetService = new UserService(testRepository);
+
+  @Test
+  public void testExistsWithValidUser() {
+    User savedUser =
+        new User(new UserId("000000"), new UserName("saved"), new MailAddress("saved@example.com"));
+    testRepository.save(savedUser);
+
+    User targetUser = new User(new UserId("999999"), new UserName("target"),
+        new MailAddress("target@example.com"));
+
+    assertThat(targetService.exists(savedUser)).isTrue();
+    assertThat(targetService.exists(targetUser)).isFalse();
+  }
+
+  @Test
+  public void testExistsWithNull() {
+    assertThatThrownBy(() -> {
+      targetService.exists(null);
+    }).isInstanceOf(NullPointerException.class);
+  }
+
+}
