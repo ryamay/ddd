9354a561e004ebc4355e8c6d2f893cf3e8ef365c
Ryuji Yamamoto <yamamoto.ryuji.links@gmail.com>
2020-04-14 23:46:32 +0900

    MODIFY:UserDataTestをassertJを使用するよう変更

---
+++ idddd-web/src/test/java/com/yamari/idddd/application/users/UserDataTest.java
@@ -0,0 +1,39 @@
+package com.yamari.idddd.application.users;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import com.yamari.idddd.domain.models.users.MailAddress;
+import com.yamari.idddd.domain.models.users.User;
+import com.yamari.idddd.domain.models.users.UserId;
+import com.yamari.idddd.domain.models.users.UserName;
+
+public class UserDataTest {
+
+  @Test
+  public void testUserDataWithString() {
+    String id = "id";
+    String name = "name";
+    String mailAddress = "address@example.com";
+    UserData target = new UserData(id, name, mailAddress);
+
+    assertThat(target.getId()).isEqualTo(id);
+    assertThat(target.getName()).isEqualTo(name);
+    assertThat(target.getMailAddress()).isEqualTo(mailAddress);
+  }
+
+  @Test
+  public void testUserDataWithUserDomainModel() {
+    String id = "id";
+    String name = "name";
+    String mailAddress = "address@example.com";
+
+    User user = new User(new UserId(id), new UserName(name), new MailAddress(mailAddress));
+
+    UserData target = new UserData(user);
+
+    assertThat(target.getId()).isEqualTo(id);
+    assertThat(target.getName()).isEqualTo(name);
+    assertThat(target.getMailAddress()).isEqualTo(mailAddress);
+  }
+
+}
